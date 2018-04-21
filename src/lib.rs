3b715d66f042b1ea7c69ef9a567ef50f73a80e2d
Adam Kewley <contact@adamkewley.com>
2018-04-21 19:53:34 +0100

    Mostly working implementation

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,8 @@ use std::fs::File;
 use memmap::Mmap;
 use byteorder::{LittleEndian, ByteOrder};
 use std::str;
+use std::path::PathBuf;
+use std::io::Write;
 
 const NUM_FOOTER_BYTES: usize = 8;
 const DAT_FILE_MIN_SIZE: usize = NUM_FOOTER_BYTES;
@@ -14,13 +16,15 @@ const TREE_ENTRY_HEADER_SIZE: usize = 4;
 const TREE_ENTRY_FOOTER_SIZE: usize = 13;
 const TREE_ENTRY_MIN_SIZE: usize =
     TREE_ENTRY_HEADER_SIZE + TREE_ENTRY_FOOTER_SIZE;
+const PATH_SEPARATOR: char = '\\';
 
+#[derive(Debug)]
 pub struct TreeEntry {
-    filename: String,
+    filename: PathBuf,
     is_compressed: bool,
-    decompressed_size: u32,
-    packed_size: u32,
-    offset: u32,
+    decompressed_size: usize,
+    packed_size: usize,
+    offset: usize,
 }
 
 pub struct TreeEntryIterator<'a> {
@@ -76,10 +80,14 @@ pub fn read_tree_entry(data: &[u8]) -> io::Result<(TreeEntry, usize)> {
         return Err(err);
     }
 
-    let filename_bytes = (&data[TREE_ENTRY_HEADER_SIZE + 1..filename_len]).to_owned();
-
-    let filename = match String::from_utf8(filename_bytes) {
-        Ok(s) => Ok(s),
+    let filename = match  str::from_utf8(&data[TREE_ENTRY_HEADER_SIZE + 1..filename_len]) {
+        Ok(s) => {
+            let mut filename = PathBuf::new();
+            for el in s.split(PATH_SEPARATOR) {
+                filename.push(el);
+            }
+            Ok(filename)
+        },
         Err(_) => {
             let err_kind = std::io::ErrorKind::InvalidData;
             let err_msg = format!("cannot decode filename as ASCII");
@@ -95,25 +103,41 @@ pub fn read_tree_entry(data: &[u8]) -> io::Result<(TreeEntry, usize)> {
     let tree_entry = TreeEntry {
         filename,
         is_compressed: data[0] > 0,
-        decompressed_size: LittleEndian::read_u32(&footer_data[1..5]),
-        packed_size: LittleEndian::read_u32(&footer_data[5..9]),
-        offset: LittleEndian::read_u32(&footer_data[9..13]),
+        decompressed_size: LittleEndian::read_u32(&footer_data[1..5]) as usize,
+        packed_size: LittleEndian::read_u32(&footer_data[5..9]) as usize,
+        offset: LittleEndian::read_u32(&footer_data[9..13]) as usize,
 
     };
 
     Ok((tree_entry, total_len))
 }
 
-pub fn run(dat_path_str: &String) -> io::Result<()> {
-    let dat_path = Path::new(dat_path_str);
-    let dat_file = File::open(dat_path)?;
-    let dat_file = unsafe { Mmap::map(&dat_file)? };
+pub fn list_contents(dat_path_str: &str) -> io::Result<()> {
+    let dat_file = mmap_dat(&dat_path_str)?;
+    let tree_entries = find_entries(&dat_file)?;
 
+    for tree_entry in tree_entries {
+        match tree_entry {
+            Ok(entry) => println!("{}", entry.filename.to_str().unwrap()),
+            Err(e) => println!("{}", e),
+        }
+    }
+
+    Ok(())
+}
+
+fn mmap_dat(dat_path_str: &str) -> io::Result<Mmap> {
+    let dat_path = Path::new(&dat_path_str);
+    let dat_file = File::open(dat_path)?;
+    unsafe { Mmap::map(&dat_file) }
+}
+
+fn find_entries(dat_file: &[u8]) -> io::Result<TreeEntryIterator> {
     let len = dat_file.len();
 
     if len < DAT_FILE_MIN_SIZE {
         let err_kind = std::io::ErrorKind::InvalidData;
-        let err_msg = format!("{}: is too small: must be at least 8 bytes long", dat_path_str);
+        let err_msg = format!("is too small: must be at least 8 bytes long");
         let err = std::io::Error::new(err_kind, err_msg);
         return Err(err);
     }
@@ -123,7 +147,7 @@ pub fn run(dat_path_str: &String) -> io::Result<()> {
 
     if file_size != len {
         let err_kind = std::io::ErrorKind::InvalidData;
-        let err_msg = format!("{}: size on disk ({}) doesn't match size from dat_file size field ({})", dat_path_str, len, file_size);
+        let err_msg = format!("size on disk ({}) doesn't match size from dat_file size field ({})", len, file_size);
         let err = std::io::Error::new(err_kind, err_msg);
         return Err(err);
     }
@@ -133,7 +157,7 @@ pub fn run(dat_path_str: &String) -> io::Result<()> {
 
     if tree_size + NUM_FOOTER_BYTES > file_size {
         let err_kind = std::io::ErrorKind::InvalidData;
-        let err_msg = format!("{}: size on disk ({}) is too small to fit the tree data ({}) plus footers", dat_path_str, len, tree_size);
+        let err_msg = format!("size on disk ({}) is too small to fit the tree data ({}) plus footers", len, tree_size);
         let err = std::io::Error::new(err_kind, err_msg);
         return Err(err);
     }
@@ -141,13 +165,64 @@ pub fn run(dat_path_str: &String) -> io::Result<()> {
     let tree_entries_data =
         &dat_file[len-tree_size-4..len-NUM_FOOTER_BYTES];
 
-    let tree_entries = read_tree_entries(tree_entries_data);
+    Ok(read_tree_entries(tree_entries_data))
+}
 
-    for tree_entry in tree_entries {
-        match tree_entry {
-            Ok(entry) => println!("{}", entry.filename),
-            Err(e) => println!("{}", e),
+pub fn extract(dat_path: &str, output_path: &str) -> io::Result<()> {
+    let output_path = Path::new(&output_path);
+
+    if !output_path.exists() {
+        let err_kind = std::io::ErrorKind::NotFound;
+        let err_msg = format!("{}: no such directory", output_path.to_str().unwrap());
+        let err = std::io::Error::new(err_kind, err_msg);
+        return Err(err);
+    } else if !output_path.is_dir() {
+        let err_kind = std::io::ErrorKind::InvalidInput;
+        let err_msg = format!("{}: not a directory", output_path.to_str().unwrap());
+        let err = std::io::Error::new(err_kind, err_msg);
+        return Err(err);
+    } else {
+        let dat_path = Path::new(&dat_path);
+        let dat_file = mmap_dat(dat_path.to_str().unwrap())?;
+        let tree_entries = find_entries(&dat_file)?;
+
+        for tree_entry in tree_entries {
+            let tree_entry = tree_entry?;
+            extract_entry(&dat_file, &output_path, tree_entry)?;
         }
+
+        Ok(())
+    }
+}
+
+fn extract_entry(dat_data: &[u8], output_dir: &Path, entry: TreeEntry) -> io::Result<()> {
+    let dat_data = if entry.offset > dat_data.len() {
+        let err_kind = std::io::ErrorKind::InvalidData;
+        let err_msg = format!("{}: start offset ({}) is outside of the data's bounds", entry.filename.to_str().unwrap(), entry.offset);
+        let err = std::io::Error::new(err_kind, err_msg);
+        Err(err)
+    } else if entry.offset + entry.packed_size > dat_data.len() {
+        let err_kind = std::io::ErrorKind::InvalidData;
+        let err_msg = format!("{}: end index({}) is outside the data's bounds", entry.filename.to_str().unwrap(), entry.offset + entry.packed_size);
+        let err = std::io::Error::new(err_kind, err_msg);
+        Err(err)
+    } else {
+        Ok(&dat_data[entry.offset..entry.offset+entry.packed_size])
+    }?;
+
+    let output_path = output_dir.join(&entry.filename);
+
+    if let Some(parent) = output_path.parent() {
+        std::fs::create_dir_all(parent)?;
+        println!("{}", parent.to_str().unwrap());
+    }
+
+    if output_path.exists() {
+        println!("{}: already exists: skipping", output_path.to_str().unwrap());
+    } else {
+        let mut output_file = std::fs::File::create(&output_path)?;
+        println!("{}", output_path.to_str().unwrap());
+        output_file.write(dat_data)?;
     }
 
     Ok(())
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,64 @@
 extern crate fo2dat;
+extern crate clap;
 
 use std::error::Error;
+use clap::App;
+use clap::Arg;
+use std::env;
 
-const USAGE: &str = "fo2dat [OPTION...] [FILE]...";
+const APP_NAME: &str = "fo2dat";
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    let matches = App::new(APP_NAME)
+        .about("A Fallout 2 DAT archive utility")
+        .arg(Arg::with_name("extract")
+                 .short("x")
+                 .long("extract")
+                 .help("extract files from a DAT2 archive")
+                 .takes_value(false))
+        .arg(Arg::with_name("file")
+            .short("f")
+            .long("--file")
+            .value_name("DAT2_FILE")
+            .help("use file")
+            .takes_value(true))
+        .arg(Arg::with_name("list")
+                 .short("t")
+                 .long("list")
+                 .help("list the contents of a DAT2 archive"))
+        .arg(Arg::with_name("directory")
+            .short("-C")
+            .long("--directory")
+            .help("change to dir before performing any operations")
+            .takes_value(true))
+        .get_matches();
 
-    match args.len() {
-        2 => {
-            let file_name = &args[1];
-            if let Err(e) = fo2dat::run(file_name) {
-                print_and_die(e.description(), 1);
-            }
+    let maybe_file = matches.value_of("file");
+
+    if let None = maybe_file {
+        print_and_die("must provide file arg (-f)", 1);
+    }
+
+    let should_extract = matches.is_present("extract");
+    let should_list = matches.is_present("list");
+
+    if should_extract && should_list {
+        print_and_die("you cannot specify more than one '-xt' option", 1);
+    } else if should_list {
+        if let Err(e) = fo2dat::list_contents(maybe_file.unwrap()) {
+            print_and_die(e.description(), 1);
         }
-        _ => {
-            print_and_die(USAGE, 1);
+    } else {
+        let cwd = env::current_dir().unwrap();
+        let output_path = matches.value_of("directory").unwrap_or(cwd.to_str().unwrap());
+        if let Err(e) = fo2dat::extract(maybe_file.unwrap(), output_path) {
+            print_and_die(e.description(), 1);
         }
     }
 }
 
 fn print_and_die(s: &str, exit_code: i32) {
-    println!("{}", s);
+    println!("{}: {}", APP_NAME, s);
+    println!("Try '{} --help' for more information", APP_NAME);
     std::process::exit(exit_code);
 }
