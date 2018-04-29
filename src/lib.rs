2f85263ab2b7e32444956dabc40a58e62e195605
Adam Kewley <contact@adamkewley.com>
2018-04-29 16:06:53 +0100

    Refactored codebase into library + CLI frontend, ready for reuse in (e.g.) a crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,32 @@
-extern crate memmap;
 extern crate byteorder;
 extern crate flate2;
 
 use std::io;
-use std::path::Path;
-use std::fs::File;
-use memmap::Mmap;
 use byteorder::{LittleEndian, ByteOrder};
 use std::str;
 use std::path::PathBuf;
-use std::io::Write;
-use flate2::read::ZlibDecoder;
 use std::io::Error;
 use std::io::ErrorKind;
+use std::ops::Range;
 
-/// The first state of the DAT parser, reached by calling `DatSectionsFoundState::from_bytes` on a
-/// byte slice.
-///
-/// DAT files are split into:
-///
-/// - A data section (variable size - calculated by knowing the offsets of the tree section)
-/// - A tree section (variable size - calculated by reading the DAT file's footers)
-/// - Metadata (num files, tree size, file size - known as part of the DAT spec)
-///
-/// Knowing the offsets + size of each section is a requirement for parsing the more-useful
-/// higher-level entries (e.g. files in the DAT). This state represents when the parser has
-/// calculated those values.
 #[allow(dead_code)]
-pub struct DatSectionsFoundState<'a> {
-    file_data: &'a [u8],
+struct DatTopLevelStructure {
+    data: Range<usize>,
     num_files: usize,
-    tree_entry_data: &'a [u8],
+    tree: Range<usize>,
     file_size: usize,
 }
 
-impl <'a> DatSectionsFoundState<'a> {
+impl DatTopLevelStructure {
 
-    /// Attempt to enter the first parsing state by parsing the supplied raw byte slice as DAT2 data.
-    ///
-    /// This operation is cheap, because it's only parsing section offsets, which only requires
-    /// reading small, scattered fields from the supplied byte slice.
-    pub fn from_bytes(bytes: &[u8]) -> io::Result<DatSectionsFoundState> {
+    fn parse(dat_data: &[u8]) -> io::Result<Self> {
         const NUM_FILES_BYTES: usize = 4;
         const TREE_SIZE_BYTES: usize = 4;
         const FILE_SIZE_BYTES: usize = 4;
         const NUM_FOOTER_BYTES: usize = TREE_SIZE_BYTES + FILE_SIZE_BYTES;
         const MIN_SIZE: usize = NUM_FILES_BYTES + NUM_FOOTER_BYTES;
 
-        let len = bytes.len();
+        let len = dat_data.len();
 
         if len < MIN_SIZE {
             let err_msg = format!("is too small: must be at least 8 bytes long");
@@ -55,7 +34,7 @@ impl <'a> DatSectionsFoundState<'a> {
         }
 
         let file_size =
-            LittleEndian::read_u32(&bytes[len-FILE_SIZE_BYTES..]) as usize;
+            LittleEndian::read_u32(&dat_data[len-FILE_SIZE_BYTES..]) as usize;
 
         if file_size != len {
             let err_msg = format!("size of data ({}) doesn't match size from the dat_file size field ({})", len, file_size);
@@ -65,7 +44,7 @@ impl <'a> DatSectionsFoundState<'a> {
         let tree_end = len - NUM_FOOTER_BYTES;
 
         let tree_size =
-            LittleEndian::read_u32(&bytes[tree_end..][..TREE_SIZE_BYTES]) as usize - TREE_SIZE_BYTES;
+            LittleEndian::read_u32(&dat_data[tree_end..][..TREE_SIZE_BYTES]) as usize - TREE_SIZE_BYTES;
 
         if tree_size > tree_end {
             let err_msg = format!("size of data ({}) is too small to fit tree entries", len);
@@ -82,57 +61,61 @@ impl <'a> DatSectionsFoundState<'a> {
         let num_files_start = tree_start - NUM_FILES_BYTES;
 
         let num_files =
-            LittleEndian::read_u32(&bytes[num_files_start..][..NUM_FILES_BYTES]) as usize;
+            LittleEndian::read_u32(&dat_data[num_files_start..][..NUM_FILES_BYTES]) as usize;
 
-        Ok(DatSectionsFoundState {
-            file_data: &bytes[0..num_files_start],
+        Ok(DatTopLevelStructure {
+            data: (0..num_files_start),
             num_files,
-            tree_entry_data: &bytes[tree_start..tree_end],
+            tree: (tree_start..tree_end),
             file_size,
         })
     }
+}
 
-    /// Returns an iterator that emits each `TreeEntry` parsed from the original input byte slice.
-    pub fn iter_tree_entries(&'a self) -> Box<Iterator<Item = io::Result<TreeEntry>> + 'a> {
-        let offset: usize = 0;
+/// Returns an iterator that emits tree entries found in the supplied DAT2 data.
+///
+/// The iterator will emit an `Err` if the data is invalid, followed by halting.
+pub fn iter_tree(dat_data: &[u8]) -> io::Result<TreeEntries> {
+    let top_level_structure = DatTopLevelStructure::parse(&dat_data)?;
+    Ok(TreeEntries {
+        tree_data: &dat_data[top_level_structure.tree],
+        offset: 0,
+    })
+}
 
-        let iter = (0..std::usize::MAX).scan(offset, move |offset, _| {
-            if *offset >= self.tree_entry_data.len() {
-                return None
+/// An iterator that emits `TreeEntry`s parsed from the tree section of DAT data.
+pub struct TreeEntries<'a> {
+    tree_data: &'a [u8],
+    offset: usize,
+}
+
+impl <'a> Iterator for TreeEntries<'a> {
+    type Item = io::Result<TreeEntry>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.offset >= self.tree_data.len() {
+            return None
+        }
+
+        let tree_data = &self.tree_data[self.offset..];
+
+        match TreeEntry::parse(tree_data) {
+            Ok((entry, entry_size)) => {
+                self.offset += entry_size;
+                Some(Ok(entry))
+            },
+            Err(e) => {
+                // halts iteration
+                self.offset += std::usize::MAX;
+                Some(Err(e))
             }
-
-            match  TreeEntry::from_bytes(&self.tree_entry_data[*offset..]) {
-                Ok((entry, entry_size)) => {
-                    *offset += entry_size;
-                    Some(Ok(entry))
-                },
-                Err(e) => {
-                    *offset = std::usize::MAX; // makes next iter ret None
-                    Some(Err(e))
-                }
-            }
-        });
-
-        Box::new(iter)
-    }
-
-    /// Returns an iterator that emits each `EntryData` parsed from the original input byte slice.
-    ///
-    /// Note: This emits the entry's data as-is - it does *not* automatically decompress the data.
-    pub fn iter_data(&'a self) -> Box<Iterator<Item = io::Result<DataEntry<'a>>> + 'a> {
-        Box::new(self.iter_tree_entries().map(move |entry| {
-            get_data(&self.file_data, entry?)
-        }))
+        }
     }
 }
 
-/// An entry, as read from the `tree_entires` section of the input DAT file.
-///
-/// `TreeEntry`s are effectively metadata about an entry in the DAT file. The *actual* data is
-/// located elsewhere in the DAT file (see: `iter_tree_entries`).
-#[allow(dead_code)]
+/// A tree entry, as parsed from the `tree_entires` section of the input DAT file.
 pub struct TreeEntry {
-    pub filename: PathBuf,
+    pub path: PathBuf,
     pub is_compressed: bool,
     pub decompressed_size: usize,
     pub packed_size: usize,
@@ -143,7 +126,7 @@ impl TreeEntry {
 
     /// Attempts to parse `data` as a tree entry. Returns the data as a `TreeEntry`, along with the
     /// number of bytes read to parse the returned `TreeEntry`.
-    pub fn from_bytes(data: &[u8]) -> io::Result<(TreeEntry, usize)> {
+    fn parse(data: &[u8]) -> io::Result<(Self, usize)> {
         const TREE_ENTRY_HEADER_SIZE: usize = 4;
         const TREE_ENTRY_FOOTER_SIZE: usize = 13;
         const TREE_ENTRY_MIN_SIZE: usize = TREE_ENTRY_HEADER_SIZE + TREE_ENTRY_FOOTER_SIZE;
@@ -160,9 +143,9 @@ impl TreeEntry {
         let total_len = TREE_ENTRY_HEADER_SIZE + filename_len + TREE_ENTRY_FOOTER_SIZE;
 
         if data.len() < total_len {
-            let err_kind = std::io::ErrorKind::InvalidData;
+            let err_kind = ErrorKind::InvalidData;
             let err_msg = format!("not enough space remaining in tree data to accommodate a filename + relevant footers");
-            let err = std::io::Error::new(err_kind, err_msg);
+            let err = Error::new(err_kind, err_msg);
             return Err(err);
         }
 
@@ -185,7 +168,7 @@ impl TreeEntry {
         let footer_data = &data[footer_start..footer_end];
 
         let tree_entry = TreeEntry {
-            filename,
+            path: filename,
             is_compressed: data[0] > 0,
             decompressed_size: LittleEndian::read_u32(&footer_data[1..5]) as usize,
             packed_size: LittleEndian::read_u32(&footer_data[5..9]) as usize,
@@ -196,107 +179,57 @@ impl TreeEntry {
     }
 }
 
+/// Returns an iterator that emits raw data entries found in the supplied DAT2 data.
+pub fn iter_data(dat_data: &[u8]) -> io::Result<DataEntries> {
+    let top_level_structure = DatTopLevelStructure::parse(dat_data)?;
+    Ok(DataEntries {
+        data_section: &dat_data[top_level_structure.data],
+        tree_entries: iter_tree(dat_data)?,
+    })
+}
+
+pub struct DataEntries<'a> {
+    data_section: &'a [u8],
+    tree_entries: TreeEntries<'a>,
+}
+
+impl <'a> Iterator for DataEntries<'a> {
+    type Item = io::Result<DataEntry<'a>>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.tree_entries.next()? {
+            Ok(tree_entry) => {
+                Some(get_data(self.data_section, tree_entry))
+            },
+            Err(e) => {
+                Some(Err(e))
+            },
+        }
+    }
+}
+
 /// A data entry (effectively, a file), as parsed from the DAT.
-///
-/// Note: The data is **not** decompressed. Users of this struct should check for zlib compression
-/// and perform decompression, if necessary.
-#[allow(dead_code)]
 pub struct DataEntry<'a> {
     pub path: PathBuf,
     pub raw_data: &'a [u8],
     pub decompressed_size: usize,
 }
 
-fn get_data<'a>(dat_data: &'a [u8], entry: TreeEntry) -> io::Result<DataEntry<'a>> {
+fn get_data<'a>(data_section_data: &'a [u8], entry: TreeEntry) -> io::Result<DataEntry<'a>> {
     let data_start = entry.offset;
     let data_end = data_start + entry.packed_size;
 
-    match dat_data.get(data_start..data_end) {
+    match data_section_data.get(data_start..data_end) {
         Some(entry_data) => {
             Ok(DataEntry {
-                path: entry.filename,
+                path: entry.path,
                 raw_data: entry_data,
                 decompressed_size: entry.decompressed_size,
             })
         },
         None => {
-            let err_msg = format!("{}: data range ({}-{}) is out of bounds", entry.filename.to_str().unwrap(), data_start, data_end);
+            let err_msg = format!("{}: data range ({}-{}) is out of bounds", entry.path.to_str().unwrap(), data_start, data_end);
             Err(Error::new(ErrorKind::InvalidData, err_msg))
         }
     }
-}
-
-/// Lists the contents of a DAT file to the standard output.
-pub fn list_contents(dat_path_str: &str) -> io::Result<()> {
-    let raw_dat_data = mmap_dat_file(dat_path_str)?;
-    let dat_sections = DatSectionsFoundState::from_bytes(&raw_dat_data)?;
-    let tree_entries = dat_sections.iter_tree_entries();
-
-    for tree_entry in tree_entries {
-        let tree_entry = tree_entry?;
-        println!("{}", tree_entry.filename.to_str().unwrap());
-    }
-
-    Ok(())
-}
-
-fn mmap_dat_file(dat_path_str: &str) -> io::Result<Mmap> {
-    let dat_path = Path::new(&dat_path_str);
-    if dat_path.exists() {
-        let dat_file = File::open(dat_path)?;
-        unsafe { Mmap::map(&dat_file) }
-    } else {
-        let err_msg = format!("{}: no such file", dat_path_str);
-        return Err(Error::new(ErrorKind::NotFound, err_msg));
-    }
-}
-
-/// Extract all entries in a DAT file located at `dat_path` to `output_dir`
-pub fn extract_all_entries(dat_path: &str, output_dir: &str) -> io::Result<()> {
-    let output_dir = Path::new(&output_dir);
-
-    if !output_dir.exists() {
-        let err_msg = format!("{}: no such directory", output_dir.to_str().unwrap());
-        return Err(Error::new(ErrorKind::NotFound, err_msg));
-    } else if !output_dir.is_dir() {
-        let err_msg = format!("{}: not a directory", output_dir.to_str().unwrap());
-        return Err(Error::new(ErrorKind::InvalidInput, err_msg));
-    } else {
-        let raw_dat_data = mmap_dat_file(dat_path)?;
-        let sections_found_state = DatSectionsFoundState::from_bytes(&raw_dat_data)?;
-        let entries_data = sections_found_state.iter_data();
-
-        for entry_data in entries_data {
-            let entry_data = entry_data?;
-            let output_path = output_dir.join(&entry_data.path);
-            write_entry(entry_data.raw_data, &output_path)?;
-        }
-    }
-
-    Ok(())
-}
-
-fn write_entry(entry_data: &[u8], output_path: &Path) -> io::Result<()> {
-    if let Some(parent) = output_path.parent() {
-        std::fs::create_dir_all(parent)?;
-    }
-
-    let mut output_file = File::create(&output_path)?;
-
-    if is_zlib_compressed(&entry_data) {
-        let mut zlib_reader = ZlibDecoder::new(entry_data);
-        std::io::copy(&mut zlib_reader, &mut output_file)?;
-    } else {
-        output_file.write(entry_data)?;
-    }
-
-    Ok(())
-}
-
-/// Returns true if `data` appears to be zlib compressed.
-pub fn is_zlib_compressed(data: &[u8]) -> bool {
-    const ZLIB_FIRST_MAGIC_BYTE: u8 = 0x78;
-    const ZLIB_SECOND_MAGIC_BYTE: u8 = 0xda;
-
-    data.len() > 2 && data[0] == ZLIB_FIRST_MAGIC_BYTE && data[1] == ZLIB_SECOND_MAGIC_BYTE
 }
--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,175 @@
 extern crate fo2dat;
 extern crate clap;
+extern crate memmap;
+extern crate flate2;
 
-use std::error::Error;
 use clap::App;
 use clap::Arg;
 use std::env;
+use std::io;
+use std::path::Path;
+use std::io::ErrorKind;
+use memmap::Mmap;
+use std::fs::File;
+use flate2::read::ZlibDecoder;
+use std::io::Error;
+use std::io::Write;
 
 const APP_NAME: &str = "fo2dat";
 
-fn main() {
-    let matches = App::new(APP_NAME)
-        .about("A Fallout 2 DAT archive utility")
-        .arg(Arg::with_name("extract")
-                 .short("x")
-                 .long("extract")
-                 .help("extract files from a DAT2 archive")
-                 .takes_value(false))
-        .arg(Arg::with_name("file")
-            .short("f")
-            .long("--file")
-            .value_name("DAT2_FILE")
-            .help("use file")
-            .takes_value(true))
-        .arg(Arg::with_name("list")
-                 .short("t")
-                 .long("list")
-                 .help("list the contents of a DAT2 archive"))
-        .arg(Arg::with_name("directory")
-            .short("-C")
-            .long("--directory")
-            .help("change to dir before performing any operations")
-            .takes_value(true))
-        .get_matches();
+enum CliAction {
+    Extract,
+    List,
+}
 
-    let maybe_file = matches.value_of("file");
+struct CliArgs {
+    action: CliAction,
+    file: String,
+    ch_dir: String,
+}
 
-    if let None = maybe_file {
-        print_and_die("must provide file arg (-f)", 1);
-    }
+impl CliArgs {
+    fn parse() -> io::Result<Self> {
+        let matches = App::new(APP_NAME)
+            .about("A Fallout 2 DAT archive utility")
+            .arg(Arg::with_name("extract")
+                .short("x")
+                .long("extract")
+                .help("extract files from a DAT2 archive")
+                .takes_value(false))
+            .arg(Arg::with_name("file")
+                .short("f")
+                .long("--file")
+                .value_name("DAT2_FILE")
+                .help("use file")
+                .takes_value(true))
+            .arg(Arg::with_name("list")
+                .short("t")
+                .long("list")
+                .help("list the contents of a DAT2 archive"))
+            .arg(Arg::with_name("directory")
+                .short("-C")
+                .long("--directory")
+                .help("change to dir before performing any operations")
+                .takes_value(true))
+            .get_matches();
 
-    let should_extract = matches.is_present("extract");
-    let should_list = matches.is_present("list");
+        let should_extract = matches.is_present("extract");
+        let should_list = matches.is_present("list");
 
-    if should_extract && should_list {
-        print_and_die("you cannot specify more than one '-xt' option", 1);
-    } else if should_list {
-        if let Err(e) = fo2dat::list_contents(maybe_file.unwrap()) {
-            print_and_die(e.description(), 1);
-        }
-    } else if should_extract {
-        let cwd = env::current_dir().unwrap();
-        let output_path = matches.value_of("directory").unwrap_or(cwd.to_str().unwrap());
-        if let Err(e) = fo2dat::extract_all_entries(maybe_file.unwrap(), output_path) {
-            print_and_die(e.description(), 1);
-        }
-    } else {
-        print_and_die("must specify either either '-t' or '-x'", 1);
+        let action = if should_extract && should_list {
+            Err(Error::new(ErrorKind::InvalidInput, "you cannot specify more than one '-xt' option"))
+        } else if should_list {
+            Ok(CliAction::List)
+        } else if should_extract {
+            Ok(CliAction::Extract)
+        } else {
+            Err(Error::new(ErrorKind::InvalidInput, "must specify either either '-t' or '-x'"))
+        }?;
+
+        let file = match matches.value_of("file").map(String::from) {
+            Some(f) => Ok(f),
+            None => Err(Error::new(ErrorKind::InvalidInput, "must provide file arg (-f)")),
+        }?;
+
+        let ch_dir = match matches.value_of("directory") {
+            Some(dir) => String::from(dir),
+            None => {
+                let cwd = env::current_dir()?;
+                String::from(cwd.to_str().unwrap())
+            },
+        };
+
+        Ok(CliArgs { action, file, ch_dir })
     }
 }
 
-fn print_and_die(s: &str, exit_code: i32) {
-    eprintln!("{}: {}", APP_NAME, s);
-    eprintln!("Try '{} --help' for more information", APP_NAME);
-    std::process::exit(exit_code);
+fn main() {
+    match main_internal() {
+        Ok(()) => {},
+        Err(e) => {
+            eprintln!("{}: {}", APP_NAME, e);
+            eprintln!("Try '{} --help' for more information", APP_NAME);
+            std::process::exit(1);
+        },
+    }
+}
+
+fn main_internal() -> io::Result<()> {
+    let args = CliArgs::parse()?;
+
+    match args.action {
+        CliAction::Extract => extract_all_entries(&args.file, &args.ch_dir),
+        CliAction::List => list_entries(&args.file),
+    }
+}
+
+/// Extract all entries in a DAT file located at `dat_path` to `output_dir`
+pub fn extract_all_entries(dat_path: &str, output_dir: &str) -> io::Result<()> {
+    let output_dir = Path::new(&output_dir);
+
+    if !output_dir.exists() {
+        let err_msg = format!("{}: no such directory", output_dir.to_str().unwrap());
+        return Err(Error::new(ErrorKind::NotFound, err_msg));
+    } else if !output_dir.is_dir() {
+        let err_msg = format!("{}: not a directory", output_dir.to_str().unwrap());
+        return Err(Error::new(ErrorKind::InvalidInput, err_msg));
+    } else {
+        let data = mmap(dat_path)?;
+
+        for entry_data in fo2dat::iter_data(&data)? {
+            let entry_data = entry_data?;
+            let output_path = output_dir.join(&entry_data.path);
+            write_entry(entry_data.raw_data, &output_path)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn mmap(dat_path_str: &str) -> io::Result<Mmap> {
+    let dat_path = Path::new(&dat_path_str);
+    if dat_path.exists() {
+        let dat_file = File::open(dat_path)?;
+        unsafe { Mmap::map(&dat_file) }
+    } else {
+        let err_msg = format!("{}: no such file", dat_path_str);
+        return Err(Error::new(ErrorKind::NotFound, err_msg));
+    }
+}
+
+fn write_entry(entry_data: &[u8], output_path: &Path) -> io::Result<()> {
+    if let Some(parent) = output_path.parent() {
+        std::fs::create_dir_all(parent)?;
+    }
+
+    let mut output_file = File::create(&output_path)?;
+
+    if is_zlib_compressed(&entry_data) {
+        let mut zlib_reader = ZlibDecoder::new(entry_data);
+        std::io::copy(&mut zlib_reader, &mut output_file)?;
+    } else {
+        output_file.write(entry_data)?;
+    }
+
+    Ok(())
+}
+
+/// Returns true if `data` appears to be zlib compressed.
+pub fn is_zlib_compressed(data: &[u8]) -> bool {
+    const ZLIB_FIRST_MAGIC_BYTE: u8 = 0x78;
+    const ZLIB_SECOND_MAGIC_BYTE: u8 = 0xda;
+
+    data.len() > 2 && data[0] == ZLIB_FIRST_MAGIC_BYTE && data[1] == ZLIB_SECOND_MAGIC_BYTE
+}
+
+pub fn list_entries(dat_path: &str) -> io::Result<()> {
+    let data = mmap(dat_path)?;
+
+    for tree_entry in fo2dat::iter_tree(&data)? {
+        let tree_entry = tree_entry?;
+        println!("{}", tree_entry.path.to_str().unwrap());
+    }
+
+    Ok(())
 }
